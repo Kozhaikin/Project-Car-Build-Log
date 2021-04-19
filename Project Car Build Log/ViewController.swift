25fe21aca89017f7f09c9c58d82f9f7d7313598b
Nick Abel <abelnich@msu.edu>
2021-04-19 09:09:25 -0400

    Initial Commit

---
+++ Project Car Build Log/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project Car Build Log
+//
+//  Created by Nick Abel on 4/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
