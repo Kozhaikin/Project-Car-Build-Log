121c51fd08cc6a3b3a883d60f5b777a1ab0cd0e8
Nick Abel <Kozhaikin@gmail.com>
2021-04-20 17:27:37 -0400

    Added general settings and about section to settings page.

--- Project Car Build Log/View Controllers/SettingsViewController.swift
+++ Project Car Build Log/View Controllers/SettingsViewController.swift
@@ -10,7 +10,64 @@ import UIKit
 
 class SettingsViewController: UITableViewController {
     
+    private var showDistance: Bool = false
+    private var distanceUnit: String = "mi"
+    private var currency: String = "$"
+    private var version: String = "Version 1.0"
+    
+    @IBOutlet weak var lblShowDistance: UILabel!
+    @IBOutlet weak var swShowDistance: UISwitch!
+    @IBOutlet weak var segDistanceUnit: UISegmentedControl! // mi, km
+    @IBOutlet weak var segCurrencyUnit: UISegmentedControl! // $, £, €, ¥
+    @IBOutlet weak var lblVersion: UILabel!
+    
     override func viewDidLoad() {
+        // TODO: Read saved settings and update selection accordingly
+        self.lblVersion.text = self.version
+    }
+    
+    @IBAction func updateShowDistance(_ sender: UISwitch) {
+        self.showDistance = sender.isOn
+        save()
+    }
+    
+    @IBAction func updateDistUnit(_ sender: UISegmentedControl) {
+        switch (sender.selectedSegmentIndex) {
+            case 0:
+                self.lblShowDistance.text = "Show Miles"
+                self.distanceUnit = "mi"
+                break
+            case 1:
+                self.lblShowDistance.text = "Show Kilometers"
+                self.distanceUnit = "km"
+                break
+        default:
+            break
+        } // end switch
+        save()
+    }
+    
+    @IBAction func updateCurrencyUnit(_ sender: UISegmentedControl) {
+        // $, £, €, ¥
+        switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.currency = "$"
+            break
+        case 1:
+            self.currency = "£"
+            break
+        case 2:
+            self.currency = "€"
+            break
+        case 3:
+            self.currency = "¥"
+            break
+        default:
+            break
+        }
+    }
+    
+    func save() {
         
     }
     
