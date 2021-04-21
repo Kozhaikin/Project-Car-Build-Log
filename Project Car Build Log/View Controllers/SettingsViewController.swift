5e22f15f98169ae7b2ecfb89fb882e65a456d5bd
Nick Abel <Kozhaikin@gmail.com>
2021-04-21 14:46:40 -0400

    Added UserDefaults for saving settings to device and reading them when needed. Also added Project and Part entities to CoreData model.

--- Project Car Build Log/View Controllers/SettingsViewController.swift
+++ Project Car Build Log/View Controllers/SettingsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SettingsViewController: UITableViewController {
     
+    private let defaults = UserDefaults.standard
+    
     private var showDistance: Bool = false
     private var distanceUnit: String = "mi"
     private var currency: String = "$"
@@ -22,7 +24,53 @@ class SettingsViewController: UITableViewController {
     @IBOutlet weak var lblVersion: UILabel!
     
     override func viewDidLoad() {
-        // TODO: Read saved settings and update selection accordingly
+        // check if key-value pairs exist and create if needed else set those saved values to their class variables
+        if (defaults.object(forKey: "showDistance") == nil) {
+            defaults.set(self.showDistance, forKey: "showDistance")
+        } else {
+            self.showDistance = defaults.bool(forKey: "showDistance")
+        }
+        
+        if (defaults.object(forKey: "distanceUnit") == nil) {
+            defaults.set(self.distanceUnit, forKey: "distanceUnit")
+        } else {
+            self.distanceUnit = defaults.string(forKey: "distanceUnit") ?? "mi"
+            if (self.distanceUnit == "km") {
+                self.lblShowDistance.text = "Show Kilometers"
+            }
+        }
+        
+        if (defaults.object(forKey: "currency") == nil) {
+            defaults.set(self.currency, forKey: "currency")
+        } else {
+            self.currency = defaults.string(forKey: "currency") ?? "$"
+        }
+
+        // read the saved settings and update screen elements
+        self.swShowDistance.setOn(self.showDistance, animated: false)
+        
+        if (self.distanceUnit == "mi") {
+            segDistanceUnit.selectedSegmentIndex = 0
+        } else {
+            segDistanceUnit.selectedSegmentIndex = 1
+            self.lblShowDistance.text = "Show Kilometers"
+        }
+        
+        // $, £, €, ¥
+        switch (self.currency) {
+        case "£":
+            segCurrencyUnit.selectedSegmentIndex = 1
+            break
+        case "€":
+            segCurrencyUnit.selectedSegmentIndex = 2
+            break
+        case "¥":
+            segCurrencyUnit.selectedSegmentIndex = 3
+            break
+        default: // $
+            segCurrencyUnit.selectedSegmentIndex = 0
+        }
+        
         self.lblVersion.text = self.version
     }
     
@@ -65,10 +113,13 @@ class SettingsViewController: UITableViewController {
         default:
             break
         }
+        save()
     }
     
     func save() {
-        
+        defaults.set(self.showDistance, forKey: "showDistance")
+        defaults.set(self.distanceUnit, forKey: "distanceUnit")
+        defaults.set(self.currency, forKey: "currency")
     }
     
     
